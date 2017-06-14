89e04e8ed13077bd4351aae038b91627432f91db
Jc <jcjimenezz23@gmail.com>
2017-06-14 18:21:16 -0400

    function that will end the game & prompt the user their score

--- basicCardIndex.js
+++ basicCardIndex.js
@@ -31,12 +31,34 @@ function playRound(currentIndex, currentScore, cardArray){
 } 
 
 
-
 //function that will prompt the user a question
+function askQuestion(currentIndex, currentScore, cardArray){
+	var card = cardArray[currentIndex];
 
-
+	inquirer.prompt([{
+		type: "input",
+		name: "text",
+		message: card.front + "\nAnswer: "
+	}]).then(function(answer){
+		if (answer.text === card.back) {
+			console.log("Correct");
+			currentScore ++;
+		}
+		else {
+			console.log("Incorrect, the answer is " + card.back);
+		}
+		currentIndex ++;
+		playRound(currentIndex, currentScore, cardArray);
+	})
+}
 
 
 //function that will end the game
+
+function endGame(score) {
+	console.log("The game is over thanks for playing, your score is " + score);
+}
+
+start();
 
 
