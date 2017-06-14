ad33a2a99af68cdb9d8187c808b590947faa8ca6
Jc <jcjimenezz23@gmail.com>
2017-06-14 17:27:52 -0400

    addicreated beginner files for the basic card flashcards

---
+++ basicCardIndex.js
@@ -0,0 +1,4 @@
+//requiring my basic card constructor, card data & inquirer package
+var BasicCard = require("./Basic.js");
+var cardData = require("./basic.json");
+var inquirer = require("inquirer");
